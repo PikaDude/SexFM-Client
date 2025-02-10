0ba71df58ca6a6ef73ee2faa721129809dfa6d53
PikaDude <mail@pikadude.me>
2025-02-10 18:28:15 +1100

    replace error page with a simpler/smaller one

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="bg-white p-2 h-screen">
+        <p class="text-4xl">
+            Error {{ error?.statusCode || 'Unknown' }}
+        </p>
+        <a @click="handleError">
+            Clear error
+        </a>
+        <br>
+        <code>{{ error }}</code>
+    </div>
+</template>
+
+<script lang="ts">
+import { object } from 'vue-types';
+import type { NuxtError } from '#app';
+
+export default defineComponent({
+    props: {
+        error: object<NuxtError>(),
+    },
+    methods: {
+        handleError() {
+            clearError({ redirect: '/' });
+        },
+    },
+});
+</script>
